313120538d26490dd6750cab3fc7f7698a004f0c
Varin Nair <varinnair@gmail.com>
2018-02-17 22:08:38 -0800

    new commit

--- AudFeatExtraction.py
+++ AudFeatExtraction.py
@@ -2,50 +2,33 @@ from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import scipy as sp
 
 frame_size = 0.050*2
 frame_stepsize = 0.025*2
 
 labels = {}
 datadir = "C:/Users/varin/Desktop/New Audio Files/"
+data = np.zeros((100,34*5))
 for i, dirname in enumerate(os.listdir(datadir)):
-	speaker = dirname 
-	
-	if "lie" in filename:
-		#labels.append((filename, 1))
-		labels[filename] = 1
-	else:
-		labels[filename] = 0
+        if "lie" in dirname:
+                labels[dirname] = 1
+        else:
+                labels[dirname] = 0
 
-	[Fs, x] = audioBasicIO.readAudioFile(datadir+dirname)
-	#we might want to play with the timeframe here - as it is this is giving us up to ~1.5k frames for our sequences
-	#speaker_feat = speaker_features[dirname]
-	st_features = audioFeatureExtraction.stFeatureExtraction(x, Fs, frame_size*Fs, frame_stepsize*Fs)
-	num_features, num_windows = st_features.shape #34, 388
+        [Fs, x] = audioBasicIO.readAudioFile(datadir+dirname)
+        st_features = audioFeatureExtraction.stFeatureExtraction(x, Fs, frame_size*Fs, frame_stepsize*Fs)
+        num_features, num_windows = st_features.shape
 
-
-
-
-	new_features = np.zeros((num_features, num_windows))
-	for i in range(num_features):
-		new_features[i] = (st_features[i] - speaker_feat[i])/speaker_feat[i]
-	st_features = np.concatenate((st_features, new_features))
-	features[filename] = st_features.tolist()
-	total += 1
-	print(i)
-
-
-"""
-
-dirname = "C:/Users/varin/Desktop/ML audio to vector/New Recording 1-1 1.wav"
-
-
-
-[Fs, x] = audioBasicIO.readAudioFile(dirname)
-F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
-
-
-
-
-print (F.shape) #34, 388
-"""
+        for j in range(num_features):
+                avg = np.mean(st_features[j])
+                min_value = np.min(st_features[j])
+                max_value = np.max(st_features[j])
+                std = np.std(st_features[j])
+                skew = sp.stats.skew(st_features[j])
+            
+                segment = np.hstack((avg,min,max,std,skew))
+                start = j*5
+                end = j*5 + 5
+                data[i,start:end] = segment
